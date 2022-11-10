e58c4071d20819d37293f461527920124919b987
Jesse <ftsjess@gmail.com>
2022-11-10 12:57:49 -0500

    header and footer

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+function Footer(props) {
+    return (
+        <footer>
+            <a href="https://github.com/Jesse115">GitHub</a>
+            <a href="https://www.linkedin.com/in/jesse-ashford-21648b235/">LinkedIn</a>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+function Header() {
+    return (
+        <header>
+            <h1>Jesse Ashford</h1>
+        </header>
+    );
+}
+
+export default Header;
